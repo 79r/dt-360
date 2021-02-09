1316b57987eb7d2c091df0f9182656166e61522e
79r <riski.web.id@zoho.com>
2021-02-09 09:26:01 +0700

    Remove unused component

--- src/App.js
+++ src/App.js
@@ -12,20 +12,6 @@ const useStyles = createUseStyles({
     padding: 0,
     fontFamily: "Roboto, sans-serif",
   },
-  myButton: {
-    color: "green",
-    margin: {
-      // jss-plugin-expand gives more readable syntax
-      top: 5, // jss-plugin-default-unit makes this 5px
-      right: 0,
-      bottom: 0,
-      left: "1rem",
-    },
-    "& span": {
-      // jss-plugin-nested applies this to a child span
-      fontWeight: "bold", // jss-plugin-camel-case turns this into 'font-weight'
-    },
-  },
 });
 
 function App() {
